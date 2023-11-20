68eb441c9100893564e45bd09aa662a23461239d
MattClarke873 <matclarke216@hotmail.com>
2023-11-20 13:14:25 +0000

    update password

--- js/popPassword.js
+++ js/popPassword.js
@@ -1,35 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Focus on the password input when the page loads
-    document.getElementById('password').focus();
 
-    // Add event listener to the password input for Enter key
-    document.getElementById('password').addEventListener('keyup', function (event) {
-      if (event.key === 'Enter') {
-        checkPassword();
-      }
-    });
+  document.addEventListener('DOMContentLoaded', function () {
+      // Show the pop-up when the page loads
+      showPopup();
   });
 
-  function checkPassword() {
-    // Get the entered password
-    var enteredPassword = document.getElementById('password').value;
-
-    // Check if the password is correct
-    if (enteredPassword === "Boobs") {
-      // Show the pop-up when the password is correct
-      showPopup();
-    } else {
-      // Handle incorrect password (you can customize this part)
-      alert("Incorrect password. Access denied.");
-    }
-  }
-
   function showPopup() {
-    var popupContainer = document.getElementById('popup-container');
-    popupContainer.style.display = 'flex';
+      var popupContainer = document.getElementById('popup-container');
+      popupContainer.style.display = 'flex';
   }
 
   function closePopup() {
-    var popupContainer = document.getElementById('popup-container');
-    popupContainer.style.display = 'none';
+      var popupContainer = document.getElementById('popup-container');
+      popupContainer.style.display = 'none';
+  }
+
+  function checkPassword() {
+      var passwordInput = document.getElementById('password');
+      var password = passwordInput.value;
+
+      // Replace 'yourSecretPassword' with your actual password
+      if (password === 'Boobs') {
+          closePopup();
+      } else {
+          alert('Incorrect password. Please try again.');
+          // Clear the input field for the next attempt
+          passwordInput.value = '';
+      }
   }
